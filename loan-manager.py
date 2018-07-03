063574c6e564e5edf77dcfcb787d3dc91c9326de
kazitor <32972585+kazitor@users.noreply.github.com>
2018-07-03 10:15:23 +1000

    change indentation and set window sizes
    Now indented with four spaces rather than tabs

--- loan-manager.py
+++ loan-manager.py
@@ -2,45 +2,47 @@ from tkinter import *
 from loan import Loan
 
 class Application(Frame):
-	"""Root window"""
-	def __init__(self, master):
-		super(Application, self).__init__(master)
-		self.master=master
-		self.grid()
-		self.add_widgets()
-	def add_widgets(self):
-		Button(self,text="Open file").grid(row=0)
-		for i,loan in enumerate(self.getloans()):
-			Label(self, text=loan.name).grid(row=i+1,column=0)
-			Button(self,text="Edit",command=lambda loan=loan: self.editloan(loan)).grid(row=i+1,column=1)
-		Button(self,text="New loan",command=self.editloan).grid(row=i+2,column=0)
-	def getloans(self) -> list:
-		# temporary
-		return [Loan("technical debt",100000,1),Loan("My house",9999899,10)]
-	def editloan(self,loan=None):
-		""" Open a window for managing a single loan """
-		window = LoanEditWindow(self.master,loan)
+    """Root window"""
+    def __init__(self, master):
+        super(Application, self).__init__(master)
+        self.master=master
+        self.grid()
+        self.add_widgets()
+    def add_widgets(self):
+        Button(self,text="Open file").grid(row=0)
+        for i,loan in enumerate(self.getloans()):
+            Label(self, text=loan.name).grid(row=i+1,column=0)
+            Button(self,text="Edit",command=lambda loan=loan: self.editloan(loan)).grid(row=i+1,column=1)
+            Button(self,text="New loan",command=self.editloan).grid(row=i+2,column=0)
+    def getloans(self) -> list:
+        # temporary
+        return [Loan("technical debt",100000,1),Loan("My house",9999899,10)]
+    def editloan(self,loan=None):
+        """ Open a window for managing a single loan """
+        window = LoanEditWindow(self.master,loan)
+        window.geometry("300x200")
 
 class LoanEditWindow(Toplevel):
-	"""Window for editing or creating a loan"""
-	def __init__(self, master,loan=None):
-		super(LoanEditWindow, self).__init__(master)
-		self.master = master
-		self.loan = loan
-		self.grid()
-		self.add_widgets()
-	def add_widgets(self):
-		self.fields={}
-		Label(self,text="Loan total").grid(row=0,column=0)
-		self.fields['Total'] = Entry(self)
-		for i,field in enumerate(self.fields):
-			self.fields[field].grid(row=i,column=1)
-		if self.loan:
-			self.wm_title("Editing %s" % self.loan.name)
-		else:
-			self.wm_title("Create new loan")
+    """Window for editing or creating a loan"""
+    def __init__(self, master,loan=None):
+        super(LoanEditWindow, self).__init__(master)
+        self.master = master
+        self.loan = loan
+        self.grid()
+        self.add_widgets()
+    def add_widgets(self):
+        self.fields={}
+        Label(self,text="Loan total").grid(row=0,column=0)
+        self.fields['Total'] = Entry(self)
+        for i,field in enumerate(self.fields):
+            self.fields[field].grid(row=i,column=1)
+        if self.loan:
+            self.wm_title("Editing %s" % self.loan.name)
+        else:
+            self.wm_title("Create new loan")
 
 root = Tk()
 root.title("Loan manager")
+root.geometry("400x200")
 app = Application(root)
 root.mainloop()
