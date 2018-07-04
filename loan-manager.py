9eb38120dd9afd1ffcc97c23092d7246d1caaf49
kazitor <32972585+kazitor@users.noreply.github.com>
2018-07-04 12:45:54 +1000

    more GUIing

--- loan-manager.py
+++ loan-manager.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from loan import Loan
+import loan
 
 class Application(Frame):
     """Root window"""
@@ -15,33 +15,41 @@ class Application(Frame):
         for i,loan in enumerate(self.getloans()):
             Label(self, text=loan.name).grid(row=i+1,column=0)
             Button(self,text="Edit",command=lambda loan=loan: self.editloan(loan)).grid(row=i+1,column=1)
-            Button(self,text="New loan",command=self.editloan).grid(row=i+2,column=0)
+        Button(self,text="New loan",command=self.editloan).grid(row=i+2,column=0)
     def getloans(self) -> list:
         # temporary
-        return [Loan("technical debt",100000,1),Loan("My house",9999899,10)]
+        return [loan.Loan("technical debt",100000),loan.CompoundLoan("Mortgage",9999899,1,10)]
     def editloan(self,loan=None):
         """ Open a window for managing a single loan """
         window = LoanEditWindow(self.master,loan)
 
 class LoanEditWindow(Toplevel):
     """Window for editing or creating a loan"""
-    def __init__(self, master,loan=None):
+    def __init__(self, master,loanobject=None):
         super(LoanEditWindow, self).__init__(master)
         self.master = master
-        self.loan = loan
+        self.loan = loanobject
+
+        self.forcompound = BooleanVar()
+        self.forcompound.set( type(loanobject) == loan.CompoundLoan )
+        self.fields=[]
+        
         self.geometry("300x200")
         self.grid()
         self.add_widgets()
     def add_widgets(self):
-        self.fields={}
-        Label(self,text="Loan total").grid(row=0,column=0)
-        self.fields['Total'] = Entry(self)
-        for i,field in enumerate(self.fields):
-            self.fields[field].grid(row=i,column=1)
+        Radiobutton(self,text="Simple", variable=self.forcompound, command=self.set_fields, value=False).grid(row=0,column=0)
+        Radiobutton(self,text="Compound", variable=self.forcompound, command=self.set_fields, value=True).grid(row=0,column=1)
+        self.set_fields()
         if self.loan:
             self.wm_title("Editing %s" % self.loan.name)
         else:
             self.wm_title("Create new loan")
+    def set_fields(self):
+        for i,name in enumerate(('Name','Loan total')):
+            labelfield = Label(self,text=name).grid(row=i+1,column=0)
+            entryfield = Entry(self).grid(row=i+1,column=1)
+            self.fields.append((labelfield, entryfield))
 
 root = Tk()
 app = Application(root)
