c9ff9bd0672c3b5776402e418dc92fa76952e5bb
kazitor <32972585+kazitor@users.noreply.github.com>
2018-08-03 09:16:52 +1000

    Interest object

--- loan.py
+++ loan.py
@@ -5,21 +5,33 @@ class Term(object):
     def __init__(self, startdate=None, enddate=None):
         self.startdate = startdate
         self.enddate = enddate
+    def timeleft(self):
+        return self.enddate - datetime.date.today
+
+class Interest(object):
+    """Interest information of a compound loan"""
+    def __init__(self, rate, period, compound_period = None):
+        self.rate = rate
+        self.period = period
+        self.compound_period = compound_period if compound_period else period
+        self.compound_rate = self.rate / (self.period / self.compound_period)
 
 class Loan(object):
     """A single loan"""
-    payed=0
     def __init__(self, name, total):
+        self.payed=0
         self.name = name
         self.total = total
 
 class CompoundLoan(Loan):
     """Loan that undergoes compound interest"""
-    def __init__(self, name, total, interest, period = (1,'y')):
+    def __init__(self, name, total, interest):
         super().__init__(name,total)
+        if type(interest) != Interest:
+            raise TypeError('interest must be an instance of Interest')
         self.interest = interest
-        self.period = period
     def amountLeft(self,repayment,periods):
+        return None
         interest=1+self.interest
         return self.total*interest**periods - repayment*interest*(1-interest**periods)/(1-interest)
 
