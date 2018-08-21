05265f3cf7450d827ec31ef370aacaca087e9aab
kazitor <32972585+kazitor@users.noreply.github.com>
2018-08-21 22:08:59 +1000

    remove old cruft
    everything is done in months now.

--- loan.py
+++ loan.py
@@ -1,13 +1,3 @@
-import datetime
-
-class Term(object):
-    """Date information of a loan"""
-    def __init__(self, startdate=None, enddate=None):
-        self.startdate = startdate
-        self.enddate = enddate
-    def timeleft(self):
-        return self.enddate - datetime.date.today
-
 class Loan(object):
     """A single loan"""
     title = 'Simple'
@@ -86,23 +76,6 @@ class Loan(object):
             return '{0:.0f}'.format(value) # don't display cents
         else:
             return '{0:.2f}'.format(value)
-
-# Classes relating to compounding loans
-
-class Interest(object):
-    """Interest information of a compound loan"""
-    def __init__(self, rate, period, compound_period = None):
-        self.rate = rate
-        self.period = period
-        self.compound_period = compound_period if compound_period else period
-        self.compound_rate = self.rate / (self.period / self.compound_period)
-
-class Period(object):
-    """Period of time suitable for finance, measured in days, months and years"""
-    def __init__(self, days=0, months=0, years=0):
-        self.days = days
-        self.months = months
-        self.years = years
 
 class CompoundLoan(Loan):
     """Loan that undergoes compound interest"""
